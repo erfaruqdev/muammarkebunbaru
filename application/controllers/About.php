52f368a43c302ea915bed18e947bd3caf532b68e
Rahman Faruq <erfaruq.dev@gmail.com>
2024-07-08 00:20:30 +0700

    style: disabled cek login in about

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -6,7 +6,7 @@ class About extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        CekLogin();
+        //CekLogin();
     }
 
     public function index()
