e9390715eac28837c3a81e92fc4bda8d26aa997f
Rahman Faruq <erfaruq.dev@gmail.com>
2025-06-18 14:33:55 +0700

    style: remove period in navbar

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -5,12 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link"><b>Tahun Periode : 1444-1445 H</b></a>
-
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
             <a class="nav-link"><b><?= dateHijriFormat(getHijri()) ?> H</b></a>
-
         </li>
     </ul>
     <!-- Right navbar links -->
