c0f0af633f8813496719574c69bd701bd06e49fe
Rahman Faruq <erfaruq.dev@gmail.com>
2026-07-17 22:48:37 +0700

    Change treasurer name

--- application/views/print/invoice.php
+++ application/views/print/invoice.php
@@ -271,7 +271,7 @@
                         Kebun Baru, <?= datetimeIDDate($data->created_at) ?> <br>
                         Bendahara <br>
                         <p style="margin-top: 60px;">
-                            <u><b>RIZAL HAFIQI</b></u>
+                            <u><b>LUKMAN YANTO</b></u>
                         </p>
                     </div>
                 </div>
@@ -374,7 +374,7 @@
                         Kebun Baru, <?= datetimeIDDate($data->created_at) ?> <br>
                         Bendahara <br>
                         <p style="margin-top: 60px;">
-                            <u><b>RIZAL HAFIQI</b></u>
+                            <u><b>LUKMAN YANTO</b></u>
                         </p>
                     </div>
                 </div>
